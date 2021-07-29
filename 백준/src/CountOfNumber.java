e9e33937ebde8c16611c3738618a4cc47233a7d0
parksangwoo <b01056721617@gmail.com>
2021-07-29 14:52:42 +0900

    ⚡ :: 일차원배열로 숫자의개수를 맞히는 문제

--- 백준/src/CountOfNumber.java
+++ 백준/src/CountOfNumber.java
@@ -7,37 +7,33 @@ public class CountOfNumber {
         int num[] = new int[10];
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("첫 번째 수");
         num1 = sc.nextInt();
-        System.out.println("두 번째 수");
         num2 = sc.nextInt();
-        System.out.println("세 번째 수");
         num3 = sc.nextInt();
 
         result = num1 * num2 * num3;
         tmp = result;
-        System.out.println(result);
         for(int i = 0;; i++) {
             tmp /= 10;
             if( tmp <= 10) {
-                System.out.println(i);
                 count = i + 2;
                 break;
             } else {
                 continue;
             }
         }
-        System.out.println(count + "얜 카운트야");
 
         array = new int[count];
         tmp2 = result;
 
-        //for(int j = 0; j < count; j++) {
-        //    for(int z = 10; z <= count; z *= 10) {
-        //        tmp2 %= z;
-        //        array[j] = tmp2;
-        //    }
-        //}
+        int spec;
+        for(int j = 0; j < count; j++) {
+            spec = tmp2 % 10;
+            array[j] = spec;
+            tmp2 /= 10;
+        }
+
+
         for(int k = 0; k < count; k++) {
             switch (array[k]) {
                 case 1: num[1] += 1;
@@ -59,7 +55,6 @@ public class CountOfNumber {
                 case 9: num[9] += 1;
                     break;
                 default: num[0] += 1;
-                    System.out.println(array[k] + "앤 디폴트");
                     break;
             }
         }
