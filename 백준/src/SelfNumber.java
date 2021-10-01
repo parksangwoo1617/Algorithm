311cbf38db46b66b77fd8f914b9cf6c2d7f6b472
parksangwoo <b01056721617@gmail.com>
2021-10-01 22:13:01 +0900

    📑 :: d(n)이 n과 n의 각 자리 수를 더하는 함수일 때, d(n)에 의해 생성되지 않는 수를 구하는 문제

---
+++ 백준/src/SelfNumber.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelfNumber {
+    public static void main(String[] args) {
+        int num = 1;
+        int tmp = 0;
+        int cnt = 1;
+        List<Integer> list = new ArrayList<>();
+        while (true) {
+            tmp = selfNumber(num);
+            if (tmp > 10000) {
+                break;
+            } else {
+                list.add(tmp);
+                num++;
+            }
+        }
+
+        for (int i = 0; i < 9993; i++, cnt++) {
+            if (list.contains(cnt)) {
+                continue;
+            } else {
+                System.out.println(cnt);
+            }
+        }
+
+    }
+
+    static int selfNumber(int num) {
+        int tmp = 0;
+        String st = Integer.toString(num);
+        String[] one = st.split("");
+        tmp += num;
+        for (int i = 0; i < one.length; i++) {
+            tmp += Integer.parseInt(one[i]);
+        }
+        return tmp;
+    }
+}
