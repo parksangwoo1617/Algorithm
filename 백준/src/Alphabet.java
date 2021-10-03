dc31526a65e5537a6843fcca33d03f54df76ef2f
parksangwoo <b01056721617@gmail.com>
2021-10-03 21:49:39 +0900

    📑 :: 알파벳의 위치를 출력하는 문제

---
+++ 백준/src/Alphabet.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class Alphabet {
+    public static void main(String[] args) {
+        String[] alphabet = new String[26];
+        char add = 97;
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String[] result = str.split("");
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < alphabet.length; i++) {
+            alphabet[i] = String.valueOf(add);
+            add++;
+        }
+
+        for (int j = 0; j < alphabet.length; j++) {
+            boolean bo = false;
+            for (int z = 0; z < result.length; z++) {
+                if (alphabet[j].equals(result[z])) {
+                    System.out.print(z + " ");
+                    bo = true;
+                    break;
+                }
+            }
+            if (bo == false) {
+                System.out.print(-1 + " ");
+            }
+        }
+    }
+}
