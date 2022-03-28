626db5a30383d9996670081998de4f5d8efdadd9
parksangwoo <b01056721617@gmail.com>
2022-03-28 13:35:03 +0900

    두 배열을 비교해 값을 찾는 문제

---
+++ 알고리즘/src/프로그래머스/Solution.java
@@ -0,0 +1,17 @@
+package 프로그래머스;
+
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for(int i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+        return participant[participant.length - 1];
+    }
+}
